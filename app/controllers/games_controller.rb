0900a00187ef3f60f8c200db3981146180bf9297
Maggie hum <hum.maggie@gmail.com>
2023-02-20 16:19:42 -0500

    able to compare if real word

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,7 @@
+require 'json'
+require 'open-uri'
+
+
 class GamesController < ApplicationController
   def new
     @letters = ('a'..'z').to_a.sample(10)
@@ -5,5 +9,22 @@ class GamesController < ApplicationController
   end
 
   def score
+    @letters = params[:letters]
+    @answer = params[:answer]
+
+    url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
+    json = URI.open(url).read
+    response_hash = JSON.parse(json)
+
+    # raise
+
+    @output = if response_hash['found'] == true
+                'good'
+              else
+                "Sorry #{@answer} does not seem to be a valid english word..."
+              end
+    # if @answer.include?(@letters)
+    #   'good'
+    # end
   end
 end
